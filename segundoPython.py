19f413e6ed72322c213b7187542de82719689a36
Adrian <ayumijo16@gmail.com>
2024-04-18 20:40:38 -0500

    commit inicial

---
+++ segundoPython.py
@@ -0,0 +1,16 @@
+n1=int(input('Ingrese el primer número:'))
+n2=int(input('Ingrese el segundo número:'))
+
+suma=n1+n2
+
+resta=n2-n1
+
+producto=n1*n2
+
+potencia=n1**n2
+
+cuadradoperfecto=n1**2+2*(n1*n2)+n2**2
+
+total=suma+resta+potencia+producto+cuadradoperfecto
+
+print('Los resultados son: ',suma,', ', resta,', ',producto,', ',potencia,', ',cuadradoperfecto,', ',total,'.')
